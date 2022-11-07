abccf56748c9800820b7e46cd52826d620a030d8
Paulo Santos <tm.paulo.santos@gmail.com>
2022-11-07 16:29:14 +0000

    Update compile.sh
    Removes useless assignment.

--- compile.sh
+++ compile.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-BUILD_OUTPUT=$(ng build |& tail -1)
-
 IFS='-'
 
-read -a STR_ARR <<< "$BUILD_OUTPUT"
+read -a STR_ARR <<< $(ng build |& tail -1)
 
 BUILD_AT=`echo ${STR_ARR[0]-} ${STR_ARR[1]-} ${STR_ARR[2]-} | sed 's/Build at: //g' | sed 's/ /-/g' | sed 's/.$//'`
 VERSION_HASH=`echo ${STR_ARR[3]} | sed 's/ Hash: //g' | sed 's/ //g'`
