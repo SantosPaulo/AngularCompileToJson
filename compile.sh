7ba3f346895996f3a4a68f6f7f9f3be7c2c8d0df
Paulo Santos <tm.paulo.santos@gmail.com>
2022-12-20 09:34:40 +0000

    Updated compile.sh
    Combined multiple sed's condition at once.

--- compile.sh
+++ compile.sh
@@ -4,8 +4,8 @@ IFS='-'
 
 read -a STR_ARR <<< $(ng build |& tail -1)
 
-BUILD_AT=`echo ${STR_ARR[0]-} ${STR_ARR[1]-} ${STR_ARR[2]-} | sed 's/Build at: //g' | sed 's/ /-/g' | sed 's/.$//'`
-VERSION_HASH=`echo ${STR_ARR[3]} | sed 's/ Hash: //g' | sed 's/ //g'`
+BUILD_AT=`echo ${STR_ARR[0]-} ${STR_ARR[1]-} ${STR_ARR[2]-} | sed 's/Build at: //g; s/ /-/g; s/.$//'`
+VERSION_HASH=`echo ${STR_ARR[3]} | sed 's/ Hash: //g; s/ //g'`
 TIME_SPENT=`echo ${STR_ARR[4]} | sed 's/ Time: //g'`
 
 echo "{ \"hash\": \"${VERSION_HASH}\", \"build_at\": \"${BUILD_AT}\", \"time\": \"${TIME_SPENT}\" }" | jq .
